691ad0a6e735b7b25232836358e819aa1ff4a7eb
Ian Rahman <ian@evergreenlabs.io>
2017-06-29 11:21:17 -0400

    add RunCell

--- Victory/ViewControllers/ListViewController.swift
+++ Victory/ViewControllers/ListViewController.swift
@@ -7,15 +7,47 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let runCellIdentifier = "runCell"
+    let realm = try! Realm()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(RunCell.self, forCellReuseIdentifier: runCellIdentifier)
     }
     
     
 
 }
 
+// MARK: - TableView Delegate
+
+extension ListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+// MARK: - TableView Data Source
+
+extension ListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return realm.objects(Run.self).count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: runCellIdentifier, for: indexPath) as? RunCell
+        else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+}
---
+++ Victory/Views/RunCell.swift
@@ -0,0 +1,15 @@
+//
+//  RunCell.swift
+//  Victory
+//
+//  Created by Ian Rahman on 6/29/17.
+//  Copyright © 2017 Evergreen Labs. All rights reserved.
+//
+
+import UIKit
+
+class RunCell: UITableViewCell {
+    
+    
+    
+}
