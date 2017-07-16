f0ee9602532990e7a7dadbf912d00d4de4b65b4c
Ian Rahman <ian@evergreenlabs.io>
2017-07-16 16:08:37 -0400

    add tests for Date extensions

--- VictoryTests/FoundationExtensionsTests.swift
+++ VictoryTests/FoundationExtensionsTests.swift
@@ -11,19 +11,45 @@ import XCTest
 
 class FoundationExtensionsTests: XCTestCase {
     
+    var date: Date!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        date = formatter.date(from: "1989/12/30 12:34:56")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        date = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // MARK: - Date
+    
+    func testDateExtensions() {
+        let year = 1989
+        let month = 12
+        let weekday = "Saturday"
+        let monthAsString = "December"
+        let day = 30
+        let hour = 12
+        let minute = 34
+        let second = 56
+        let prettyDate = "12/30/1989"
+        let id = 19891230123456
+        
+        XCTAssert(date.year == year, "Year does not format correctly")
+        XCTAssert(date.month == month, "Month does not format correctly")
+        XCTAssert(date.weekday == weekday, "Weekday does not format correctly")
+        XCTAssert(date.monthAsString == monthAsString, "Month as String does not format correctly")
+        XCTAssert(date.day == day, "Day does not format correctly")
+        XCTAssert(date.hour == hour, "Hour does not format correctly")
+        XCTAssert(date.minute == minute, "Minute does not format correctly")
+        XCTAssert(date.second == second, "Second does not format correctly")
+        XCTAssert(date.prettyDate == prettyDate, "Pretty Date does not format correctly")
+        XCTAssert(date.id == id, "ID does not format correctly")
     }
     
 }
